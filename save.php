75aaf4905cb3665ec316d63076e556e2e60c47e1
jungsangkr <92278688+jungsangkr@users.noreply.github.com>
2022-04-28 17:03:50 +0400

    Create save.php

---
+++ save.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <title></title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php include 'db.php'; ?>
+            <?php include 'api.php'; ?>
+            <?php
+                    if(!empty($_POST['name']) && !empty($_POST['id'])) {
+                        $name = $_POST['name'];
+                        $id = $_POST['id'];
+                        saveExchange($db, $name, $id);
+                    }   else {
+                        echo "<h1>Ошибка сохранения данных</h1>";
+                    }
+                    
+             ?>
+        </div>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
