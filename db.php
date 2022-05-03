d6620616d1c1d38c2b7041444b4ccc3c9dacf6ac
jungsangkr <92278688+jungsangkr@users.noreply.github.com>
2022-05-03 05:48:04 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
-$user = "u1387845_db";
-$password = "tW6vJ6tP5n";
+$user = "db";
+$password = "ПАРОЛЬ";
 
 try {
-	$db = new PDO("mysql:host=server253.hosting.reg.ru;dbname=u1387845_db", $user, $password);
+	$db = new PDO("localhost;dbname=БАЗА", $user, $password);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
