48584b9570c1d920bf3baac22b761547a676a1b4
jungsangkr <92278688+jungsangkr@users.noreply.github.com>
2022-05-03 05:51:06 +0400

    Update api.php

--- api.php
+++ api.php
@@ -1,33 +1,22 @@
+<?php include 'db.php'; ?>
 <?php
-
 function getAllTest($db) {
-
     $sql = "SELECT * FROM hyaa_directions";
     $result = array();
     $stmt = $db->prepare($sql);
-    //print_r($stmt);
     $stmt->execute();
-   /*echo "<pre>";
-        print_r($res);
-    echo "</pre>";*/
-    
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $result[$row['id']] = $row;
     }
-
     return $result;
-    
 }
-
-
-
-function saveExchange($db, $new_parser_actions_give, $new_parser_actions_get, $id) {
-    $sql = "UPDATE hyaa_direction
+function saveExchange($db, $give, $get, $id) {
+    $sql = "UPDATE hyaa_directions
             SET new_parser_actions_give = :new_parser_actions_give, new_parser_actions_get = :new_parser_actions_get
             WHERE id = :id";
-    $stmt =$this->$db->prepare($sql);
-    $stmt->bindValue(':new_parser_actions_give', $new_parser_actions_give, PDO::PARAM_STR);
-    $stmt->bindValue(':new_parser_actions_get', $new_parser_actions_get, PDO::PARAM_STR);
+    $stmt =$db->prepare($sql);
+    $stmt->bindValue(':new_parser_actions_give', $give, PDO::PARAM_STR);
+    $stmt->bindValue(':new_parser_actions_get', $get, PDO::PARAM_STR);
     $stmt->bindValue(':id', $id, PDO::PARAM_STR);
     $stmt->execute();
 }
